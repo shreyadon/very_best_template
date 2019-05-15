db2a27e9c742ca3bc1fb0ef920955b84595f37da
Raghu Betina <raghu@firstdraft.com>
2019-05-15 21:07:41 +0000

    Validate presence of bookmarks.user_id

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,4 +5,6 @@ class Bookmark < ApplicationRecord
 
   # Validations
 
+  validates :user_id, :presence => true
+
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe Bookmark, type: :model do
     end
 
     describe "Validations" do
+
+    it { should validate_presence_of(:user_id) }
       
     end
 end
