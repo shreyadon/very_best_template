0c7cf5041850c0d8897dc0c182178324fdcf49d6
Raghu Betina <raghu@firstdraft.com>
2019-05-15 21:08:36 +0000

    Only bookmark user can access show page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :current_user_must_be_bookmark_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_bookmark_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_bookmark_user
     bookmark = Bookmark.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
